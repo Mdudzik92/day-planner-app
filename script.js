b47bafc5bd472b3ea43d7574a25595f48b991cdd
Matt Dudzik <mdudzik92@gmail.com>
2020-08-04 16:16:42 -0500

    Updated save feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $(".btn").on("click", function() {
+$(document).ready(function () {
+    $(".btn").on("click", function () {
         var element = $(this).parent().siblings(".col-sm-8")
         var time = $(this).parent().siblings(".col-sm-2")
         element = $(element)
@@ -11,5 +11,16 @@ $(document).ready(function() {
         console.log(value)
         console.log(timeValue)
         localStorage.setItem(timeValue, value)
+        var now = moment()
+        var moment = moment().format();
+    })
+
+        $(".col-sm-8").each(function() {
+            console.log(this)
+            var hour =   $(this).prev().text();
+            console.log(hour)
+            var inputText =  localStorage.getItem(hour)
+            console.log(hour,inputText)
+            $(this).children().val(inputText)
     })
 })
