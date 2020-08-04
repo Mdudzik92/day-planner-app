2da7a4082e20dcde5ddfeebca027c265a0ef8f0d
Matt Dudzik <mdudzik92@gmail.com>
2020-08-04 16:59:07 -0500

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    var jumboTronEl = document.querySelector(".jumbotron");
+    var currentTime = moment().format("LLLL");
+    jumbotronEl.append(currentTime)
+    
     $(".btn").on("click", function () {
         var element = $(this).parent().siblings(".col-sm-8")
         var time = $(this).parent().siblings(".col-sm-2")
@@ -15,12 +19,12 @@ $(document).ready(function () {
         var moment = moment().format();
     })
 
-        $(".col-sm-8").each(function() {
-            console.log(this)
-            var hour =   $(this).prev().text();
-            console.log(hour)
-            var inputText =  localStorage.getItem(hour)
-            console.log(hour,inputText)
-            $(this).children().val(inputText)
+    $(".col-sm-8").each(function() {
+        console.log(this)
+        var hour =   $(this).prev().text();
+        console.log(hour)
+        var inputText =  localStorage.getItem(hour)
+        console.log(hour,inputText)
+        $(this).children().val(inputText)
     })
 })
